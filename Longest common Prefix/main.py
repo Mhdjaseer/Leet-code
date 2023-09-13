760f8d9c84e5ceedd9c831a2848f4ca235e7d00d
jaseer j <jassjaseerdazzils2000@gmail.com>
2023-09-13 23:25:37 +0530

    finish

--- Longest common Prefix/main.py
+++ Longest common Prefix/main.py
@@ -2,27 +2,43 @@
 # strs=['ab','abcd','cd']
 strs=['cir','car']
 
-ans=""
-v=sorted(strs)
-first=v[0]
-first=(first[:2])
+class Solution:
+    def longestCommonPrefix(self,v):
+        ans=""
+        v=sorted(v)
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans 
+obj=Solution()
+result=obj.longestCommonPrefix(strs)
+print(result)
+#     if s[:2] == first:
+#          print(s[:2])
+#          count+=1
 
-# print(first)
-count=0
-for s in v:
-    # print(s)
-    if s[:2] == first or s[:1]==first:
-        #  print(s[:2])
-         count+=1
+
+# while count < len(v):
+#     current_str = v[count]
+#     print(current_str,"no repeat")
+#     for i in range(len(current_str)):
+#         print(current_str[i])
+#     count += 1
+
+
+
 
 # print(count)
-if len(v)== count:
-     print(first,"this ")  
+# if len(v)== count:
+#      print(first,"this ")  
 
-else:
-     print("empty")
+# else:
+#      print("empty")
 
-last=v[-1]
+# last=v[-1]
 # print(first)
 # print(last)
 # def prefix(first,last):
