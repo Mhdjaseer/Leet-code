10ddc722cba375a9a20e0b16c61879ec9b82db86
jaseer j <jassjaseerdazzils2000@gmail.com>
2023-09-18 19:11:28 +0530

    linked list node

--- Valid Parentheses/main.py
+++ Valid Parentheses/main.py
@@ -1,9 +1,13 @@
+
+#error not compleated 
 class Solution:
     def isValid(self,s:str)->bool:
         my_list=['{}','()',[]]
-        for i in range(len(s)-1):
+        for i in range(len(s)):
             substring=s[i:i+1]
-            print(substring)
+            for j in range(len(my_list)):
+                if substring and (substring)==my_list[i]:
+                    print("hi")
 
 
 obj=Solution()
