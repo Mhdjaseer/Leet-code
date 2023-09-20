dd6204eca15573f9d6577a0d14045a8ed6cd3cf7
jaseer j <jassjaseerdazzils2000@gmail.com>
2023-09-20 13:06:20 +0530

    linked list sort

---
+++ merge_tow_sorted_list/main.py
@@ -0,0 +1,97 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, )
+            current = current.next
+        print("None")
+
+    def search(self, target):
+        current = self.head
+        while current:
+            if current.data == target:
+                return True
+            current = current.next
+        return False
+
+    def remove(self, target):
+        if not self.head:
+            return
+        if self.head.data == target:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == target:
+                current.next = current.next.next
+                return
+            current = current.next
+
+
+def merged_linked_list(list1,list2):
+    if not list1.head:
+        return list2
+    if not list2.head:
+        return list1
+    
+    merged_list=LinkedList()
+    current1=list1.head
+    current2=list2.head
+
+
+    while current1 and current2:
+        if current1.data<current2.data:
+            merged_list.append(current1.data)
+            current1=current1.next 
+        else:
+            merged_list.append(current2.data)
+            current2=current2.next  
+            
+
+# Usage example:
+my_list = LinkedList()
+my_list.append(1)
+my_list.append(2)
+my_list.append(4)
+# my_list.append(5)
+
+my_second_list=LinkedList()
+my_second_list.append(1)
+my_second_list.append(3)
+my_second_list.append(4)
+
+
+
+my_list.display()  # Output: 1 -> 2 -> 3 -> None
+# print(my_list.search(2))  # Output: True
+# my_list.remove(2)
+my_second_list.display()  # Output: 1 -> 3 -> None
+
+#######################################
+class merge:
+    def __init__(self,list1,list2) -> None:
+        pass
+    
+    
+
+
+obj=merge(list2=my_list,list1=my_second_list)
+    
---
+++ merge_tow_sorted_list/readme.md
@@ -0,0 +1,6 @@
+# Merge  are given the heads of two sorted linked [list1] and [list2]
+
+![Alt text](image.png)
+```
+
+```
